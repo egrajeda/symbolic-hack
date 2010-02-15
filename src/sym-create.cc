3d5dafc69181d79a33ea2ba986d2124c8bffaab2
matlab <matlab@LABCCBB43.(none)>
2010-02-15 15:48:40 -0600

    Added a 'syms' function that behaves like MATLAB.
    * It still doesn't support declaration of multiple variables at the
      same time.
    
    * You don't have to do:
        > x = sym('x');
      anymore, instead you can do:
        > syms x
      which is equivalent.

--- src/sym-create.cc
+++ src/sym-create.cc
@@ -97,6 +97,35 @@ Create an object of type symbol\n")
   return retval;
 }
 
+DEFUN_DLD (syms,args, , 
+"-*- texinfo -*-\n\
+Create an object of type symbol\n")
+{
+  mlock ();
+  octave_value retval;
+  int nargin = args.length ();
+
+  if (nargin != 1)
+    {
+      error("one argument expected\n");
+      return octave_value ();
+    }
+  try 
+    {
+      GiNaC::symbol xtmp(args(0).string_value());
+      octave_ex x(xtmp);
+      retval = octave_value(new octave_ex(x));
+      set_variable_value(args(0).string_value(), retval);
+    }
+  catch (std::exception &e)
+    {
+      error (e.what ());
+      retval = octave_value ();
+    }
+
+  return retval;
+}
+
 // an ex DLD function might be nice 
 
 DEFUN_DLD (ex_matrix, args, ,
